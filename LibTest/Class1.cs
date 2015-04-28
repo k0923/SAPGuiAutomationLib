c2429217666b3fd7e2ccd3ead21daec6df83c08f
Yang <kav7860@gmail.com>
2015-04-28 20:24:12 +0800

    Commit

---
+++ LibTest/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace LibTest
+{
+    public class Test
+    {
+        public Test() { }
+
+        public void ShowMessage(string message)
+        {
+            System.Windows.Forms.MessageBox.Show(message);
+        }
+    }
+}
--- SAPGuiAutomationLib/RecordStep.cs
+++ SAPGuiAutomationLib/RecordStep.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.CodeDom;
+using SAPTestRunTime;
 
 namespace SAPGuiAutomationLib
 {
@@ -18,5 +20,58 @@ namespace SAPGuiAutomationLib
         public string ActionName { get; set; }
 
         public object[] ActionParams { get; set; }
+
+        public CodeStatement GetCodeStatement()
+        {
+            switch(Action)
+            {
+                case BindingFlags.SetProperty:
+                    return getAssignStatement();
+                case BindingFlags.InvokeMethod:
+                    return getInvokeStatement();
+                default:
+                    return null;
+            }
+        }
+
+        private CodeAssignStatement getAssignStatement()
+        {
+            CodeAssignStatement asStatement = new CodeAssignStatement();
+
+            CodeMethodInvokeExpression genericMethod = new CodeMethodInvokeExpression(
+                      new CodeMethodReferenceExpression(
+                         new CodeVariableReferenceExpression("SAPTestHelper.Current"),
+                             "GetElementById",
+                                 new CodeTypeReference[] {
+                                    new CodeTypeReference(CompInfo.Type),
+                                       }), new CodePrimitiveExpression(CompInfo.Id));
+
+
+            asStatement.Left = new CodePropertyReferenceExpression(genericMethod,ActionName);
+
+            asStatement.Right = new CodePrimitiveExpression(ActionParams[0]);
+            return asStatement;
+        }
+
+        private CodeExpressionStatement getInvokeStatement()
+        {
+            
+            CodeExpressionStatement statement = new CodeExpressionStatement();
+            CodeMethodInvokeExpression genericMethod = new CodeMethodInvokeExpression(
+                      new CodeMethodReferenceExpression(
+                         new CodeVariableReferenceExpression("SAPTestHelper.Current"),
+                             "GetElementById",
+                                 new CodeTypeReference[] {
+                                    new CodeTypeReference(CompInfo.Type),
+                                       }), new CodePrimitiveExpression(CompInfo.Id));
+            CodeExpression[] paras = new CodeExpression[ActionParams.Count()];
+            for(int i =0;i<ActionParams.Count();i++)
+            {
+                paras[i] = new CodePrimitiveExpression(ActionParams[i]);
+            }
+            CodeMethodInvokeExpression method = new CodeMethodInvokeExpression(genericMethod, ActionName,paras);
+            statement.Expression = method;
+            return statement;
+        }
     }
 }
