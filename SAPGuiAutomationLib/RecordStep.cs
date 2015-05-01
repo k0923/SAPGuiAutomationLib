298632b5582d2aeb5b5f44045576071c175fe693
Yang <kav7860@gmail.com>
2015-05-01 12:05:21 +0800

    update

--- SAPGuiAutomationLib/RecordStep.cs
+++ SAPGuiAutomationLib/RecordStep.cs
@@ -21,66 +21,10 @@ namespace SAPGuiAutomationLib
 
         public object[] ActionParams { get; set; }
 
-        public CodeStatement GetCodeStatement()
-        {
-            switch(Action)
-            {
-                case BindingFlags.SetProperty:
-                    return getAssignStatement();
-                case BindingFlags.InvokeMethod:
-                    return getInvokeStatement();
-                default:
-                    return null;
-            }
-        }
+        
 
-        private CodeAssignStatement getAssignStatement()
-        {
-            CodeAssignStatement asStatement = new CodeAssignStatement();
-
-            CodeMethodInvokeExpression genericMethod = new CodeMethodInvokeExpression(
-                      new CodeMethodReferenceExpression(
-                         new CodeVariableReferenceExpression("SAPTestHelper.Current"),
-                             "GetElementById",
-                                 new CodeTypeReference[] {
-                                    new CodeTypeReference(CompInfo.Type),
-                                       }), new CodePrimitiveExpression(CompInfo.Id));
-
-
-            asStatement.Left = new CodePropertyReferenceExpression(genericMethod,ActionName);
-
-            asStatement.Right = new CodePrimitiveExpression(ActionParams[0]);
-            return asStatement;
-        }
-
-        private CodeExpressionStatement getInvokeStatement()
-        {
-            
-            CodeExpressionStatement statement = new CodeExpressionStatement();
-            CodeMethodInvokeExpression genericMethod = new CodeMethodInvokeExpression(
-                      new CodeMethodReferenceExpression(
-                         new CodeVariableReferenceExpression("SAPTestHelper.Current"),
-                             "GetElementById",
-                                 new CodeTypeReference[] {
-                                    new CodeTypeReference(CompInfo.Type),
-                                       }), new CodePrimitiveExpression(CompInfo.Id));
-            CodeExpression[] paras;
-            if(ActionParams != null)
-            {
-                paras = new CodeExpression[ActionParams.Count()];
-                for (int i = 0; i < ActionParams.Count(); i++)
-                {
-                    paras[i] = new CodePrimitiveExpression(ActionParams[i]);
-                }
-            }
-            else
-            {
-                paras = new CodeExpression[0];
-            }
-            
-            CodeMethodInvokeExpression method = new CodeMethodInvokeExpression(genericMethod, ActionName,paras);
-            statement.Expression = method;
-            return statement;
-        }
+        
     }
+
+    
 }
---
+++ SAPGuiAutomationLib/RecordStepExtension.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAPGuiAutomationLib
+{
+    public static class RecordStepExtension
+    {
+        public static CodeStatement GetCodeStatement(this RecordStep step)
+        {
+            switch (step.Action)
+            {
+                case BindingFlags.SetProperty:
+                    return getAssignStatement(step);
+                case BindingFlags.InvokeMethod:
+                    return getInvokeStatement(step);
+                default:
+                    return null;
+            }
+        }
+
+        private static CodeAssignStatement getAssignStatement(RecordStep step)
+        {
+            CodeAssignStatement asStatement = new CodeAssignStatement();
+
+            CodeMethodInvokeExpression genericMethod = new CodeMethodInvokeExpression(
+                      new CodeMethodReferenceExpression(
+                         new CodeVariableReferenceExpression("SAPTestHelper.Current"),
+                             "GetElementById",
+                                 new CodeTypeReference[] {
+                                    new CodeTypeReference(step.CompInfo.Type),
+                                       }), new CodePrimitiveExpression(step.CompInfo.Id));
+
+
+            asStatement.Left = new CodePropertyReferenceExpression(genericMethod, step.ActionName);
+
+            asStatement.Right = new CodePrimitiveExpression(step.ActionParams[0]);
+            return asStatement;
+        }
+
+        private static CodeExpressionStatement getInvokeStatement(RecordStep step)
+        {
+
+            CodeExpressionStatement statement = new CodeExpressionStatement();
+            CodeMethodInvokeExpression genericMethod = new CodeMethodInvokeExpression(
+                      new CodeMethodReferenceExpression(
+                         new CodeVariableReferenceExpression("SAPTestHelper.Current"),
+                             "GetElementById",
+                                 new CodeTypeReference[] {
+                                    new CodeTypeReference(step.CompInfo.Type),
+                                       }), new CodePrimitiveExpression(step.CompInfo.Id));
+            CodeExpression[] paras;
+            if (step.ActionParams != null)
+            {
+                paras = new CodeExpression[step.ActionParams.Count()];
+                for (int i = 0; i < step.ActionParams.Count(); i++)
+                {
+                    paras[i] = new CodePrimitiveExpression(step.ActionParams[i]);
+                }
+            }
+            else
+            {
+                paras = new CodeExpression[0];
+            }
+
+            CodeMethodInvokeExpression method = new CodeMethodInvokeExpression(genericMethod, step.ActionName, paras);
+            statement.Expression = method;
+            return statement;
+        }
+    }
+}
