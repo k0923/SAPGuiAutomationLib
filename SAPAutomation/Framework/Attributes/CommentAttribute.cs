230634c335d3b4bbc0f068941689434101ac0dba
Yang <kav7860@gmail.com>
2015-07-31 16:25:20 +0800

    BF

---
+++ SAPAutomation/Framework/Attributes/CommentAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SAPAutomation.Framework.Attributes
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = false)]
+    public class CommentAttribute:Attribute
+    {
+        public string Summary { get; set; }
+
+        public CommentAttribute() { }
+        
+        public CommentAttribute(string Description,int a)
+        {
+            this.Summary = Description;
+        }
+
+        
+    }
+}
